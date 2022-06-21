a50e6ecd02cc1ea394f19e378c2da933213473b9
Kamran KB <thekamrankb@gmail.com>
2022-06-21 13:41:13 +0500

    Implemented Vue Router 4 for Everyone (Lazy Loading Routes)

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
-import Brazil from '@/views/Brazil.vue'
-import Panama from '@/views/Panama.vue'
-import Hawaii from '@/views/Hawaii.vue'
-import Jamaica from '@/views/jamaica.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
-    { path: '/about', name: 'About', component: About },
-    { path: '/brazil', name: 'Brazil', component: Brazil },
-    { path: '/panama', name: 'Panama', component: Panama },
-    { path: '/hawaii', name: 'Hawaii', component: Hawaii },
-    { path: '/jamaica', name: 'Jamaica', component: Jamaica },
+    { path: '/brazil', name: 'Brazil', component: ()=> import('@/views/Brazil.vue') },
+    { path: '/panama', name: 'Panama', component: () => import('@/views/Panama.vue') },
+    { path: '/hawaii', name: 'Hawaii', component: () => import('@/views/Hawaii.vue') },
+    { path: '/jamaica', name: 'Jamaica', component: () => import('@/views/Jamaica.vue') },
 ];
 
 const router = createRouter({
