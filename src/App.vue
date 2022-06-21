fae176143a4826e860588294c6fdc2d02f184331
Kamran KB <thekamrankb@gmail.com>
2022-06-21 11:31:30 +0500

    Added Vue-Router@4 and created two routes Home and About also created separate route/index.js file

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script>
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <br>
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
+  <router-view></router-view>
 </template>
 
 <style>
