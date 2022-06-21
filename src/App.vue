f10a3f7a320b3b4c5b72e5be36bcf27f940390bd
Kamran KB <thekamrankb@gmail.com>
2022-06-21 13:51:44 +0500

    Created TheNavigation component - Vue Router 4 for Everyone (Navigation Component)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
-<script>
-</script>
-
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/panama">Panama</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-  </div>
-
+  <TheNavigation />
+  
   <div class="container">
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+import TheNavigation  from "@/components/TheNavigation.vue";
+
+export default {
+  components: {TheNavigation}
+}
+</script>
