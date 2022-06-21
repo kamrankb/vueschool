0195fb99d1dc97bcddabab27cba0772d4fb69208
Kamran KB <thekamrankb@gmail.com>
2022-06-21 21:26:53 +0500

    Vue Router 4 for Everyone (Fetching data from APIs and also implemented  method with async await functions)

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="destination">
+    <section v-if="destination" class="destination">
         <h1>{{ destination.name }}</h1>
         <div class="destination-details">
             <img :src="`/images/${destination.image}`" :alt="destination.name">
@@ -9,17 +9,31 @@
 </template>
 
 <script>
-import sourceData from '@/data.json'
-
 export default {
+    data() {
+        return {
+            destination: null
+        }
+    },
+
     computed:{
         destinationId(){
             return parseInt(this.$route.params.id);
         },
+    },
 
-        destination(){
-            return sourceData.destinations.find(destination=> destination.id=== this.destinationId);
+    methods: {
+        async initData() {
+            const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}`);
+            this.destination = await response.json();    
         }
+    },  
+
+    async created() {
+        
+        this.initData();
+
+        this.$watch(() => this.$route.params, this.initData);
     }
 }
 </script>
