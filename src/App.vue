1dda2a45b464dd5a44478e469fa04327bf65e1a1
Kamran KB <thekamrankb@gmail.com>
2023-04-15 18:56:33 +0500

    Implemented transition of views through vue-router transition

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
+<style scoped>
+/*
+  Enter and leave animations can use different
+  durations and timing functions.
+*/
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
+</style>
+
 <template>
   <TheNavigation />
   
   <div class="container">
-    <router-view :key="$route.path"></router-view>
+    <router-view v-slot="{ Component }">
+      <transition name="slide-fade">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
